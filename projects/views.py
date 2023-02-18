3697438bbb23ac2368e68ec1f43a7d5536d77de7
farokh <reza.farokh9830@gmail.com>
2023-02-18 14:15:29 +0330

    2th commit

--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('Here are our products')
+    # return HttpResponse('Here are our products')
+    return render(request, 'projects/projects.html')  # will go in setting.py find dir template and use from 
+        # content
 
 
 def project(request, pk):
-    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
+    # return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
+    return render(request, 'projects/single-project.html')
 
