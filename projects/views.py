5a12758d0e31340f8aa13262b4ff45b5d74a0c29
farokh <reza.farokh9830@gmail.com>
2023-03-07 20:39:50 +0330

    3th commit

--- projects/views.py
+++ projects/views.py
@@ -2,13 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+projectList = [
+    {
+    'id' : '1',
+    'title' : 'java project',
+    'description' : 'this is about a mobile app'
+    },
+
+    {
+    'id' : '2',
+    'title' : 'django project',
+    'description' : 'this is about backend'
+    },
+    {
+    'id' : '3',
+    'title' : 'CPP',
+    'description' : 'this is about game'
+    }
+]
+
+
 def projects(request):
     # return HttpResponse('Here are our products')
-    return render(request, 'projects/projects.html')  # will go in setting.py find dir template and use from 
+    page = 'projects'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projectList}
+    return render(request, 'projects/projects.html', context)  # will go in setting.py find dir template and use from 
         # content
 
 
 def project(request, pk):
     # return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
-    return render(request, 'projects/single-project.html')
-
+    projectObj = None
+    for i in projectList:
+        if i["id"] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
