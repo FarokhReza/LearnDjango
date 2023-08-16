38078c78236232a9e1ef5c8838e25d24a09f5d64
reza <reza.farokh9830@gmail.com>
2023-08-17 02:23:21 +0330

    end of 41

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
+from projects.views import project
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project, Review
+from projects.models import Project, Review, Tag
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -55,3 +56,37 @@ def projectVote(request, pk):
     serializer = ProjectSerializer(project, many=False)
     
     return Response(serializer.data)
+
+# @api_view(['DELETE'])
+# def removeTag(request):
+#     tagId = request.data['tag']
+#     projectId = request.data['project']
+
+#     project = Project.objects.get(id=projectId)
+#     tag = Tag.objects.get(id=tagId)
+
+#     project.tags.remove(tag)
+
+
+#     return Response('Tag was deleted!')
+
+
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from .models import Project, Tag
+
+@api_view(['DELETE'])
+def removeTag(request):
+    tagId = request.data.get('tag')  # Use .get() to avoid KeyError
+    projectId = request.data.get('project')  # Use .get() to avoid KeyError
+
+    if tagId is not None and projectId is not None:
+        try:
+            project = Project.objects.get(id=projectId)
+            tag = Tag.objects.get(id=tagId)
+            project.tags.remove(tag)
+            return Response('Tag was deleted!')
+        except (Project.DoesNotExist, Tag.DoesNotExist):
+            return Response('Project or Tag does not exist.', status=400)
+
+    return Response('Missing tag or project key in request data.', status=400)
